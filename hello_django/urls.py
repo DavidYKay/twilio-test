07d47f44eb69f72d9905e26541b0732dd9a3cb72
Randall Degges <rdegges@gmail.com>
2011-07-18 23:03:18 -0700

    Adding hello-world example project.
    It just says hello-world when called :)

---
+++ hello_django/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+message = """
+Hello, Los Angeles django users! I hope you are having a good time so far at
+the meetup! Goodbye.
+"""
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'django_twilio.views.say', {'text': message}),
+    # url(r'^hello_django/', include('hello_django.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
